bc2576aa2cc217a3da55ba144fea0eb5b31db3c7
Dikshya Khatri <157598339+dikshyakhatri8@users.noreply.github.com>
2024-08-13 23:31:28 +0200

    Update Higher_Lower_Game.py

--- Higher_Lower_Game.py
+++ Higher_Lower_Game.py
@@ -5,3 +5,33 @@
 
 
 # Generate a random account from the game data 
+
+
+
+
+# format the account data into printable format
+
+
+
+# ask user for a guess
+
+
+
+# check if user is correct
+# - get follower count of each account
+# - use if statement to check if user is correct
+
+
+# give user feedback on their guess
+
+
+
+# score keeping
+
+
+
+# make the game repeatble
+
+
+
+# making account at position B become the next account at position A
